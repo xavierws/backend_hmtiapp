247cd14a746f3bb1bf30213ce5dd0b681d4d60ba
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2020-10-22 15:58:15 +0700

    fixing feed index, change bytestring to url

--- app/Http/Resources/Feed.php
+++ app/Http/Resources/Feed.php
@@ -17,13 +17,13 @@ class Feed extends JsonResource
     {
         return [
             'id' => $this->id,
+            'title'=>$this->title,
             'caption' => $this->caption,
             'day_of_week' => $this->day_of_week,
             'image' => $this->transformImage(),
             'number_of_viewer' => $this->countViewer(),
 //            'viewer' => $this->getViewer(),
             'created_at' => $this->created_at,
-            'title'=>$this->title
         ];
     }
 
@@ -44,7 +44,7 @@ class Feed extends JsonResource
 
 //            $encodedImg = base64_encode($imgFile);
 
-            $arrayOfImg[$n] = Storage::url($filename);
+            $arrayOfImg[$n] = Storage::path($filename);
             $n++;
         }
 
