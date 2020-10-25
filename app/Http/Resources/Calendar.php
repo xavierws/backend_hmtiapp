817d1aba971af80e61c3908a2035828ae095d904
noraasteria <nora_asteria@yahoo.co.id>
2020-10-25 13:11:23 +0700

    coba

--- app/Http/Resources/Calendar.php
+++ app/Http/Resources/Calendar.php
@@ -14,6 +14,15 @@ class Calendar extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title'=>$this->title,
+            'caption' => $this->caption,
+            'day_of_week' => $this->day_of_week,
+            'image' => $this->transformImage(),
+            'number_of_viewer' => $this->countViewer(),
+//            'viewer' => $this->getViewer(),
+            'created_at' => $this->created_at,
+        ];
     }
 }
