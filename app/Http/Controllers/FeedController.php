d7359dd20bb798d202693bdaa762a04d6df6e2aa
okazaki10asdasd <rakaabadisusilo@gmail.com>
2020-10-29 23:45:32 +0700

    Update FeedController.php

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -257,7 +257,7 @@ class FeedController extends Controller
 
     /**
      * get all the feed's viewer
-     *asdasd asda sdas dasd
+     *asdasd asda sdaasd
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
